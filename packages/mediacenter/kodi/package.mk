9c8b638ae3f37dbf90fd109c6c3a9c7ade8d59d1
Manuel Hueske <m.hueske@web.de>
2016-02-27 13:11:52 +0100

    Update auf Kodi 17.0 Alpha 1 3bc9437

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="17.0-alpha1-0b65b2c"
+PKG_VERSION="17.0-alpha1-3bc9437"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
