4bb402755d3502dabb41947f0d16dcdc7aa15024
Manuel Hueske <m.hueske@web.de>
2016-03-30 00:03:21 +0200

    Revert "Revert "Update binary addons""
    This reverts commit e578a0eb6a38970f77ec1941979c2bda93698add.

--- packages/mediacenter/kodi-binary-addons/pvr.dvbviewer/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.dvbviewer/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.dvbviewer"
-PKG_VERSION="d10f17c"
+PKG_VERSION="88eb169"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
--- packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.iptvsimple"
-PKG_VERSION="1dfc354"
+PKG_VERSION="f27ae14"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
